9ca5fda556ac8c8d2332468caa5a5d056cf6508e
Ahsan Awan <ahsan.awan@fliclicks.com>
2018-03-06 01:35:05 -0500

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,30 @@ class ArticlesController<ApplicationController
 
 	end
 
+	def index
+		@articles=Article.all
+
+	end
+
 	def edit
+
+		@article=Article.find(params[:id])
 
 	end
 
 	def update
-
+		@article=Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice]="Article updated successfully"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@article=Article.find(params[:id])
+		@article.destroy
 
 	end
 
